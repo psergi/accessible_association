a1d16a9b6e8b9c310f339a80614844826e4247ab
Phil Sergi <psergi@backchannelmedia.com>
2009-01-14 11:26:37 -0500

    added multi param to accessible_association method

--- lib/accessible_association.rb
+++ lib/accessible_association.rb
@@ -1,5 +1,12 @@
 module AccessibleAssociation
-  def accessible_association(name)
+  def accessible_association(*names)
+    names.each do |name|
+      wrap_singular_association(name)
+    end
+  end
+
+  private
+  def wrap_singular_association(name)
     setter_method = "#{name}="
     original_method = instance_method(setter_method)
 
--- spec/accessible_association_spec.rb
+++ spec/accessible_association_spec.rb
@@ -19,10 +19,6 @@ describe AccessibleAssociation do
       @instance.assoc = 'original'
     end
 
-    it "should define setter method for the param passed" do
-      @instance.should respond_to(:assoc=)
-    end
-
     describe ".assoc=" do
       it "should build the associated object" do
         params = { :name => 'namezz', :status => 'complete' }
@@ -42,6 +38,62 @@ describe AccessibleAssociation do
         params = 'new'
         @instance.assoc = params
         @instance.assoc.should == 'new'
+      end
+    end
+  end
+
+  describe ".accessible_association with multiple params" do
+    before(:each) do
+      klass = Class.new(AccessibleAssociationBase) do
+        attr_accessor :assoc_one, :assoc_two
+        accessible_association :assoc_one, :assoc_two
+      end
+      @instance = klass.new
+      @instance.assoc_one = 'assoc one'
+      @instance.assoc_two = 'assoc two'
+    end
+
+    describe ".assoc_one=" do
+      it "should build the associated object" do
+        params = { :name => 'namezz', :status => 'complete' }
+        @instance.should_receive(:build_assoc_one).with(params)
+        @instance.assoc_one = params
+        @instance.assoc_one.should == 'assoc one'
+      end
+
+      it "should not call the original method when passed a hash" do
+        params = { :name => 'namezz', :status => 'complete' }
+        @instance.stub!(:build_assoc_one)
+        @instance.assoc_one = params
+        @instance.assoc_one.should == 'assoc one'
+      end
+
+      it "should call the original method if a hash is not passed" do
+        params = 'new'
+        @instance.assoc_one = params
+        @instance.assoc_one.should == 'new'
+      end
+    end
+
+    describe ".assoc_two=" do
+      it "should build the associated object" do
+        params = { :name => 'namezz', :status => 'complete' }
+        @instance.should_receive(:build_assoc_two).with(params)
+        @instance.assoc_two = params
+        @instance.assoc_two.should == 'assoc two'
+      end
+
+      it "should not call the original method when passed a hash" do
+        params = { :name => 'namezz', :status => 'complete' }
+        @instance.stub!(:build_assoc_two)
+        @instance.assoc_two = params
+        @instance.assoc_two.should == 'assoc two'
+      end
+
+      it "should call the original method if a hash is not passed" do
+        params = 'new'
+        @instance.assoc_two = params
+        @instance.assoc_two.should == 'new'
       end
     end
   end
